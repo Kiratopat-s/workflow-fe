c2d02ba5c33dcbe8348d70e1bbbea956fa197f70
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-05 19:35:33 +0700

    change file name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Quiz1 } from "@/components/quiz1";
 import Quiz2 from "@/components/quiz2";
 import Quiz3 from "@/components/quiz3";
-import Quiz4 from "@/components/quiz3 copy";
+import Quiz4 from "@/components/quiz4";
 
 export default function Home() {
   let name: string = "Home";
