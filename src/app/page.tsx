3f68bbdd3500fa23d51f283956fe927c735a3e42
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-05 23:16:31 +0700

    complete homework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
-import { Quiz1 } from "@/components/quiz1";
-import Quiz2 from "@/components/quiz2";
-import Quiz3 from "@/components/quiz3";
-import Quiz4 from "@/components/quiz4";
+import TableProduct, { product } from "@/components/TableProduct";
 
 export default function Home() {
+  const mockData: product[] = [
+    {
+      image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+      title: "Monitor",
+      amount: "$2500.00",
+    },
+    {
+      image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      title: "Hard Disk/SSD",
+      amount: "$2000.75",
+    },
+    {
+      image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+      title: "Monitor",
+      amount: "$2500.00",
+    },
+    {
+      image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      title: "Hard Disk/SSD",
+      amount: "$2000.75",
+    },
+  ];
   return (
     <main>
-      <Quiz1 />
-      <Quiz2 />
-      <Quiz3 />
-      <Quiz4 />
+      <TableProduct products={mockData} />
     </main>
   );
 }
