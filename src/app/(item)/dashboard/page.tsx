7b0c35d11c9509fd8daffb65c2fb562365ffa048
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-17 21:04:45 +0700

    refactor: Update AddBudget component to use axios for API request

---
+++ src/app/(item)/dashboard/page.tsx
@@ -0,0 +1,227 @@
+"use client";
+import {
+  OctagonAlert,
+  RotateCw,
+  Search,
+  SearchCheck,
+  Trash2,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useDebounce from "@/utils/debounce";
+import ItemRow from "@/components/dashboard/ItemRow";
+import toast from "react-hot-toast";
+
+function Dashboard() {
+  const [items, setItems] = useState<itemProps[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredItems, setFilteredItems] = useState<itemProps[]>([]);
+
+  const debouncedSearchQuery = useDebounce(searchQuery, 300);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    setFilteredItems(
+      items.filter((item) =>
+        item.title.toLowerCase().includes(debouncedSearchQuery.toLowerCase())
+      )
+    );
+  }, [debouncedSearchQuery, items]);
+
+  const [choosedIDs, setChoosedIDs] = useState<number[]>([]);
+
+  const handleCheckboxChange = (id: number) => {
+    setChoosedIDs((prevIDs) => {
+      if (prevIDs.includes(id)) {
+        return prevIDs.filter((choosedID) => choosedID !== id);
+      } else {
+        return [...prevIDs, id];
+      }
+    });
+  };
+
+  async function UpdateItemStatus({ status }: itemStatus, ids: number[]) {
+    const start_timer = Date.now();
+    try {
+      const res = await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/items/update/status/many`,
+        {
+          status,
+          ids,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(res.data);
+      toast.success(`Updated item status in ${Date.now() - start_timer}ms`);
+    } catch (error) {
+      toast.error("Failed to update item status");
+      console.log(error);
+    }
+  }
+
+  async function DeleteItems(ids: number[]) {
+    const start_timer = Date.now();
+    try {
+      const res = await axios.delete(
+        `${process.env.NEXT_PUBLIC_API_URL}/items/delete/many`,
+        {
+          data: { ids },
+          withCredentials: true,
+        }
+      );
+      console.log(res.data);
+      toast.success(`Deleted items in ${Date.now() - start_timer}ms`);
+    } catch (error) {
+      toast.error("Failed to delete items");
+      console.log(error);
+    }
+  }
+
+  const ApproveHandler = async () => {
+    await UpdateItemStatus({ status: "APPROVED" }, choosedIDs);
+    await fetchItems();
+    setChoosedIDs([]);
+  };
+
+  const RejectHandler = async () => {
+    await UpdateItemStatus({ status: "REJECTED" }, choosedIDs);
+    await fetchItems();
+    setChoosedIDs([]);
+  };
+
+  const DeleteHandler = async () => {
+    await DeleteItems(choosedIDs);
+    await fetchItems();
+    setChoosedIDs([]);
+  };
+
+  async function fetchItems() {
+    const startTimer = Date.now();
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/items`, {
+        withCredentials: true,
+      });
+      setItems(res.data.data);
+      console.log(res.data.data);
+      toast.success(`Fetched items in ${Date.now() - startTimer}ms`);
+    } catch (error) {
+      toast.error("Failed to fetch items");
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchItems();
+    return () => {
+      console.log("cleare");
+    };
+  }, []);
+
+  return (
+    <div className="absolute top-[10%] flex flex-col h-screen w-full justify-start items-center gap-8">
+      <div className="w-full max-w-md md:max-w-4xl lg:max-w-6xl overflow-x-auto bg-base-100 p-8 rounded-md shadow-md">
+        <label className="input input-bordered flex items-center gap-2">
+          <input
+            type="text"
+            onChange={handleSearchChange}
+            className="grow"
+            placeholder="Search"
+          />
+          <Search />
+        </label>
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label className="flex flex-col justify-center">
+                  {/* Select all checkbox */}
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        // Select all IDs if checked
+                        setChoosedIDs(filteredItems.map((item) => item.id)); // Add all IDs here
+                      } else {
+                        setChoosedIDs([]);
+                      }
+                    }}
+                  />
+                </label>
+              </th>
+              <th className="flex gap-2">
+                <div
+                  onClick={() => {
+                    fetchItems();
+                  }}
+                  className="btn btn-ghost"
+                >
+                  <RotateCw />
+                </div>
+                <div className="relative">
+                  <div className="absolute flex gap-2">
+                    {choosedIDs.length > 0 && (
+                      <>
+                        <div
+                          onClick={ApproveHandler}
+                          className="btn btn-success text-base-300"
+                        >
+                          <SearchCheck />
+                          Approve
+                        </div>
+                        <div
+                          onClick={RejectHandler}
+                          className="btn btn-warning text-base-300"
+                        >
+                          <OctagonAlert />
+                          Reject
+                        </div>
+                        <div
+                          onClick={DeleteHandler}
+                          className="btn btn-error text-base-300"
+                        >
+                          <Trash2 />
+                          Delete
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <p className=" hidden badge badge-info"></p>
+                <p className=" hidden badge badge-error"></p>
+                <p className=" hidden badge badge-success"></p>
+                <p className=" hidden badge badge-warning"></p>
+              </th>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Budget</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredItems.map((item: itemProps) => (
+              <ItemRow
+                key={item.id}
+                item={item}
+                choosedIDs={choosedIDs}
+                handleCheckboxChange={handleCheckboxChange}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/dashboard/ItemRow.tsx
@@ -0,0 +1,82 @@
+import { ItemStatus } from "@/enum/Item";
+import { Pencil, ShieldCheck } from "lucide-react";
+import Link from "next/link";
+
+interface itemProps {
+  id: number;
+  title: string;
+  amount: number;
+  quantity: number;
+  status: "APPROVED" | "PENDING" | "REJECTED";
+}
+
+interface ItemRowProps {
+  item: itemProps;
+  choosedIDs: number[];
+  handleCheckboxChange: (id: number) => void;
+}
+
+const ItemRow: React.FC<ItemRowProps> = ({
+  item,
+  choosedIDs,
+  handleCheckboxChange,
+}) => {
+  return (
+    <tr key={item.id} className="hover:bg-base-200">
+      <th>
+        <label>
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={() => handleCheckboxChange(item.id)}
+            checked={choosedIDs.includes(item.id)}
+          />
+        </label>
+      </th>
+      <td>{item.id}</td>
+      <td>
+        {item.status === "PENDING" ? (
+          <Link
+            href={`/update/${item.id}`}
+            className="cursor-pointer hover:underline flex gap-2 align-baseline"
+          >
+            <div className="self-center">
+              <Pencil className="scale-50 text-warning" />
+            </div>
+            <p className="self-center">{item.title}</p>
+          </Link>
+        ) : (
+          <div className="cursor-pointer hover:underline flex gap-2 align-baseline">
+            <div className="self-center">
+              <ShieldCheck className="scale-[70%] text-accent" />
+            </div>
+            <p className="self-center">{item.title}</p>
+          </div>
+        )}
+        <span className="badge badge-ghost badge-sm">x {item.quantity}</span>
+      </td>
+      <th>
+        <p
+          className={item.status === ItemStatus.REJECTED ? "line-through" : ""}
+        >
+          {item.amount.toLocaleString("TH")} THB
+        </p>
+      </th>
+      <th>
+        <p
+          className={`badge badge-${
+            item.status === ItemStatus.APPROVED
+              ? "success"
+              : item.status === ItemStatus.PENDING
+              ? "info"
+              : "warning"
+          } badge-md text-base-100`}
+        >
+          {item.status}
+        </p>
+      </th>
+    </tr>
+  );
+};
+
+export default ItemRow;
