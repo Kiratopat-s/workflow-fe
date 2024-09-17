d2fcf26cdd0ae6974771daa3acc71eba8870a971
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-17 21:00:36 +0700

    chore: Update environment variables and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "img.daisyui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
       },
     ],
   },
